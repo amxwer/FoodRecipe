a7503003922d1d14b042a281b667ac2a2d6445b6
Maxim <maksymbirilo@gmail.com>
2022-11-09 23:52:02 +0300

    Creating project

---
+++ src/main/java/com/amxwer/foodrecipe/FoodRecipeApplication.java
@@ -0,0 +1,13 @@
+package com.amxwer.foodrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amxwer/foodrecipe/FoodRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amxwer.foodrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
